f8731a13cf518bd15e027489eeca848d1fe24713
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2021-04-22 17:58:55 +0700

    env

--- src/models/index.js
+++ src/models/index.js
@@ -3,8 +3,8 @@ const { Sequelize } = require('sequelize');
 // In a real app, you should keep the database connection URL as an environment variable.
 // But for this example, we will just use a local SQLite database.
 // const sequelize = new Sequelize(process.env.DB_CONNECTION_URL);
-const sequelize = new Sequelize('crypto_store', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
 
