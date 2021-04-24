74c68007ebe5ff36b16186b147b49e02f4c4eec5
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2021-04-24 22:26:19 +0700

    allow null

--- src/models/asset.model.js
+++ src/models/asset.model.js
@@ -12,63 +12,48 @@ module.exports = (sequelize) => {
 			type: DataTypes.STRING
 		},
 		name: {
-			allowNull: false,
 			type: DataTypes.STRING,
 		},
 		type_is_crypto: {
-			allowNull: false,
 			type: DataTypes.INTEGER,
 		},
 		data_start: {
-			allowNull: false,
 			type: DataTypes.DATEONLY,
 		},
 		data_end: {
-			allowNull: false,
 			type: DataTypes.DATEONLY,
 		},
 		data_quote_start: {
-			allowNull: false,
 			type: DataTypes.DATE,
 		},
 		data_quote_end: {
-			allowNull: false,
 			type: DataTypes.DATE,
 		},
 		data_orderbook_start: {
-			allowNull: false,
 			type: DataTypes.DATE,
 		},
 		data_orderbook_end: {
-			allowNull: false,
 			type: DataTypes.DATE,
 		},
 		data_trade_start: {
-			allowNull: false,
 			type: DataTypes.DATE,
 		},
 		data_trade_end: {
-			allowNull: false,
 			type: DataTypes.DATE,
 		},
 		data_symbols_count: {
-			allowNull: false,
 			type: DataTypes.INTEGER,
 		},
 		volume_1hrs_usd: {
-			allowNull: false,
 			type: DataTypes.DOUBLE,
 		},
 		volume_1day_usd: {
-			allowNull: false,
 			type: DataTypes.DOUBLE,
 		},
 		volume_1mth_usd: {
-			allowNull: false,
 			type: DataTypes.DOUBLE,
 		},
 		price_usd: {
-			allowNull: false,
 			type: DataTypes.DOUBLE,
 		},
 		id_icon: {
